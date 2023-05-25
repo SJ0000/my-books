977f37fe24a61f9ab6e8dd9b680de8ccdb22c079
sj <slaayer60@gmail.com>
2023-05-25 22:55:16 +0900

    fix: table name 수정  user -> users

--- src/main/java/sungjin/mybooks/domain/User.java
+++ src/main/java/sungjin/mybooks/domain/User.java
@@ -2,8 +2,10 @@ package sungjin.mybooks.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "USERS")
 public class User {
 
     @Id
