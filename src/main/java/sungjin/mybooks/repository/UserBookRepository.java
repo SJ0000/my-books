bc3dbdafb1901be068bc54bc6ad3bf6aacc554d4
sj <slaayer60@gmail.com>
2023-07-05 16:09:36 +0900

    feat: findRecentUserBooksTest 추가

--- src/main/java/sungjin/mybooks/repository/UserBookRepository.java
+++ src/main/java/sungjin/mybooks/repository/UserBookRepository.java
@@ -12,11 +12,11 @@ import java.util.List;
 public interface UserBookRepository extends JpaRepository<UserBook, Long> {
 
     @Query(value = "select ub from UserBook ub join fetch ub.book where ub.user.id = :userId and ub.book.title like %:title%",
-    countQuery = "select count(ub) from UserBook ub where ub.user.id = :userId and ub.book.title like %:title%")
+            countQuery = "select count(ub) from UserBook ub where ub.user.id = :userId and ub.book.title like %:title%")
     Page<UserBook> findAllByBookTitle(Long userId, String title, Pageable pageable);
 
-    @Query(value = "select ub from UserBook ub join fetch ub.book where ub.user.id = :userId order by ub.createdAt ASC",
-            countQuery = "select count(ub) from UserBook ub where ub.user.id = :userId and ub.book.title like %:title%")
+    @Query(value = "select ub from UserBook ub join fetch ub.book where ub.user.id = :userId order by ub.createdAt DESC",
+            countQuery = "select count(ub) from UserBook ub where ub.user.id = :userId")
     Page<UserBook> findRecentUserBooks(Long userId, Pageable pageable);
 
 }
--- src/test/java/sungjin/mybooks/repository/UserBookRepositoryTest.java
+++ src/test/java/sungjin/mybooks/repository/UserBookRepositoryTest.java
@@ -8,11 +8,17 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.transaction.annotation.Transactional;
+import sungjin.mybooks.domain.BaseTimeEntity;
 import sungjin.mybooks.domain.Book;
 import sungjin.mybooks.domain.User;
 import sungjin.mybooks.domain.UserBook;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -53,11 +59,45 @@ class UserBookRepositoryTest {
         }
 
         // when
-        Page<UserBook> result = userBookRepository.findAllByBookTitle(user.getId(), "mybook", PageRequest.of(1,10));
+        Page<UserBook> result = userBookRepository.findAllByBookTitle(user.getId(), "mybook", PageRequest.of(0,10));
 
         // then
         assertThat(result.getTotalElements()).isEqualTo(3);
         assertThat(result.getTotalPages()).isEqualTo(1);
+    }
 
+    @Test
+    @DisplayName("특정 사용자가 가진 책 리스트를 최신순으로 조회한다.")
+    void findRecentUserBooksTest() throws Exception {
+        // given
+        User user = User.builder()
+                .name("user")
+                .email("alpha@beta.com")
+                .build();
+
+        userRepository.save(user);
+
+        for (int i = 1; i <= 20; i++) {
+            Book book = Book.builder()
+                    .title("mybook" + i)
+                    .build();
+            bookRepository.save(book);
+
+            UserBook userBook = UserBook.builder()
+                    .user(user)
+                    .book(book)
+                    .build();
+            ReflectionTestUtils.setField(userBook,"createdAt", LocalDateTime.of(2000,1,1,1,i));
+            userBookRepository.save(userBook);
+        }
+
+        // when
+        Page<UserBook> result = userBookRepository.findRecentUserBooks(user.getId(),  PageRequest.of(0,10));
+
+        // then
+        assertThat(result.getContent().get(0).getBook().getTitle()).isEqualTo("mybook20");
+        assertThat(result.getContent()).isSortedAccordingTo((ub1,ub2)->{
+            return ub2.getCreatedAt().compareTo(ub1.getCreatedAt());
+        });
     }
 }
