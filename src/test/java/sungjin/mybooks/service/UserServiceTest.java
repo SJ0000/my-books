0bafb2a18263f7f5dafb7d38ca6f1f2936aeb4c9
sj <slaayer60@gmail.com>
2023-10-19 21:17:35 +0900

    refactor: UserServiceTest @SpringBootTest 제거, Mocking을 이용한 단위테스트로 변경

--- src/test/java/sungjin/mybooks/service/UserServiceTest.java
+++ src/test/java/sungjin/mybooks/service/UserServiceTest.java
@@ -3,28 +3,34 @@ package sungjin.mybooks.service;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 import sungjin.mybooks.MyBooksTestUtils;
+import sungjin.mybooks.config.PasswordEncoder;
 import sungjin.mybooks.domain.User;
 import sungjin.mybooks.dto.request.SignUp;
 import sungjin.mybooks.repository.UserRepository;
 
 import java.util.Optional;
 
-@SpringBootTest
-@Transactional
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
-    @Autowired
-    UserService userService;
-
-    @MockBean
+    @Mock
     UserRepository userRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    UserService userService;
 
     @Test
     @DisplayName("회원가입시 중복 이메일일 경우 RuntimeException 발생")
