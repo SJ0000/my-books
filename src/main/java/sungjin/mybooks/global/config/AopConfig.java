40bbf98c6cb22c8531b5ebb6970452d4c62ec91f
sj <slaayer60@gmail.com>
2023-12-09 23:14:42 +0900

    feat: add AopConfig

--- src/main/java/sungjin/mybooks/global/config/AopConfig.java
+++ src/main/java/sungjin/mybooks/global/config/AopConfig.java
@@ -4,16 +4,38 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sungjin.mybooks.domain.common.aop.DomainAuthorizeAspect;
+import sungjin.mybooks.domain.common.domain.security.DomainAuthorizeManager;
+import sungjin.mybooks.domain.common.domain.security.handler.CommentAuthorizeHandler;
+import sungjin.mybooks.domain.common.domain.security.handler.LikeAuthorizeHandler;
+import sungjin.mybooks.domain.common.domain.security.handler.ReviewAuthorizeHandler;
+import sungjin.mybooks.domain.review.domain.Comment;
+import sungjin.mybooks.domain.review.domain.Like;
+import sungjin.mybooks.domain.review.domain.Review;
+import sungjin.mybooks.domain.review.repository.CommentRepository;
+import sungjin.mybooks.domain.review.repository.LikeRepository;
+import sungjin.mybooks.domain.review.repository.ReviewRepository;
+import sungjin.mybooks.domain.user.repository.UserRepository;
 
 @Configuration
 @RequiredArgsConstructor
 public class AopConfig {
 
-    private final
-
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
+    private final CommentRepository commentRepository;
+    private final LikeRepository likeRepository;
 
     @Bean
     DomainAuthorizeAspect domainAuthorizeAspect(){
-        DomainAuthorizeManager
+        DomainAuthorizeManager manager = createManager();
+        return new DomainAuthorizeAspect(manager);
+    }
+
+    private DomainAuthorizeManager createManager(){
+        DomainAuthorizeManager manager = new DomainAuthorizeManager(userRepository);
+        manager.addHandler(Review.class,new ReviewAuthorizeHandler(reviewRepository));
+        manager.addHandler(Comment.class,new CommentAuthorizeHandler(commentRepository));
+        manager.addHandler(Like.class,new LikeAuthorizeHandler(likeRepository));
+        return manager;
     }
 }
